3f816e66c43daf68f1c944f58974df6455320347
Trip4077 <bj2336@email.vccs.edu>
2019-04-12 15:51:35 -0400

    Dashboard Buttons Render Test Pass

--- baseball/src/Dashboard/Dashboard.js
+++ baseball/src/Dashboard/Dashboard.js
@@ -4,6 +4,13 @@ const Dashboard = props => {
     return(
         <div>
             <h2>Dashboard</h2>
+
+            <div>
+                <button>Strike</button>
+                <button>Ball</button>
+                <button>Fowl</button>
+                <button>Hit</button>
+            </div>
         </div>
     );
 }
--- baseball/src/Dashboard/Dashboard.test.js
+++ baseball/src/Dashboard/Dashboard.test.js
@@ -9,4 +9,28 @@ describe('Dashboard Component', () => {
     it('should render the Dashboard', () => {
         render(<Dashboard />);
     })
+
+    it('should have a strike button', () => {
+        const { getByText } = render(<Dashboard />);
+
+        getByText(/strike/i)
+    });
+
+    it('should have a ball button', () => {
+        const { getByText } = render(<Dashboard />);
+
+        getByText(/ball/i);
+    });
+
+    it('should have a fowl button', () => {
+        const { getByText } = render(<Dashboard />);
+
+        getByText(/fowl/i);
+    })
+
+    it('should have a hit button', () => {
+        const { getByText } = render(<Dashboard />);
+
+        getByText(/hit/i);
+    })
 })
